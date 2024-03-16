7db7390633921a398f911abb9fafbc6e8b20719d
Atick Faisal <atickfaisal@gmail.com>
2024-03-16 12:56:16 +0300

    Add readme for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="peter-explains",
-    version="1.0.0",
+    version="0.0.2",
     description="Linux command explanations from Peter Griffin",
     author="Atick Faisal",
     author_email="atickfaisal@gmail.com",
     packages=find_packages(),
     install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
